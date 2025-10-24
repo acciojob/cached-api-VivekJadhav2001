6dcb6c547e103f0b11740c9aa2e2fce7d9fc2740
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-24 19:41:13 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import CachedAPIUseMemo from "./CachedAPIUseMemo";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <CachedAPIUseMemo/>
     </div>
   )
 }
